ddaf542919f65a5fed0a2cf6da2af2b9eb90af86
z1399 <zhenggq@dtdream.com>
2023-08-15 14:39:46 +0800

    feat: 使用 Azure OpenAI

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -7,8 +7,23 @@ import { nanoid } from '@/lib/utils'
 
 export const runtime = 'edge'
 
+// OpenAI
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY
+// })
+
+// Azure OpenAI
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.AZURE_OPENAI_API_KEY,
+  baseOptions: {
+    headers: {
+      "api-key": process.env.AZURE_OPENAI_API_KEY,
+    },
+  },
+  basePath: `https://zheng4.openai.azure.com/openai/deployments/gpt4`,
+  defaultQueryParams: new URLSearchParams({
+    "api-version": process.env.AZURE_OPENAI_API_VERSION as string,
+  }),
 })
 
 const openai = new OpenAIApi(configuration)
